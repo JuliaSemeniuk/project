ef93854485e99defc6f0b5eee17a31764e88aa28
JuliaSemeniuk <j.semeniuk.v@gmail.com>
2020-05-30 13:07:42 +0300

    create first component

---
+++ src/GeneralComponent/01_first.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class First extends React.Component {
+    render () {
+        return (
+            <div>
+                I'm do nothing
+            </div>
+        )
+    }
+}
